4e61505e0755933a1b604f0f39be2ac4db36d9e5
william89731 <willystile@gmail.com>
2022-05-20 22:54:00 +0200

    commit script

--- scan.bash
+++ scan.bash
@@ -20,28 +20,28 @@ command -v trivy > /dev/null 2>&1 || MISSING_PACKAGES+=("trivy")
 if [[ ! -z "$MISSING_PACKAGES" ]]; then
   error "you need to install trivy!"
 fi
-info "This script scans a list of container images using Aqua Security's trivy CLI tool - https://github.com/aquasecurity/trivy"
+info "ready to scan"
 #echo ""
 sleep 2
 #info "######## List of images to scan:"
 #info "$(cat $1)"
 sleep 2
 for i in $(cat $1); do
-  info "----- Pull image -----"
+  info "#####Pull image#####"
   docker pull $i
   if [ $? == 0 ]
     then
       #echo ""
-      info "----- Show age -----"
+      info "#####Show age#####"
       docker image ls $i --format 'Image: {{.Repository}}:{{.Tag}} was created {{.CreatedSince}}'
       #echo ""
-      info "----- Scan image -----"
+      info "####Scan image####"
       trivy $i
     else
       PULLERROR=true
       #echo ""
-      warn "----- ERROR with pulling image -----"
-      info "----- -> Proceeding with next image -----"
+      warn "####ERROR pull image####"
+      info "-->####next image####"
   fi
 done
 
